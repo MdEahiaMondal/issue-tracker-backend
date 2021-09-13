5ee54403d57466ea375ff7d8d447078f02681cb4
eahiya <mdeahiamondal@gmail.com>
2021-09-13 07:34:43 +0600

    forgot and reset password on api

---
+++ config/frontend.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    "BASE_URL" => "",
+    "RESET_PASSWORD_URL" => env('RESET_PASSWORD_URL', "http://localhost:8080/password/reset")
+];
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/me', function (Request $request) {
 
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/password/forgot', [\App\Http\Controllers\Api\ForgotPasswordController::class, 'sendResetLinkEmail']);
+Route::post('/password/reset', [\App\Http\Controllers\Api\ResetPasswordController::class, 'reset']);
